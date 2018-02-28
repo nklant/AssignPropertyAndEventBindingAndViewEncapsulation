7334c51192f48d75c05367d1f7482149712c5638
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-02-28 16:27:27 +0200

    Assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalStarted(startedNumber: number) {
+    if (startedNumber % 2 === 0) {
+      this.evenNumbers.push(startedNumber);
+    }else {
+      this.oddNumbers.push(startedNumber);
+    }
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  @Output() intervalStarted = new EventEmitter<number>(); // @Output - so it can be caught outside this component; intervalStarted is the event name, since there is no alias
+  interval;
+  num = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalStarted.emit(this.num + 1);
+      this.num++;
+    }, 1000);
+  }
+
+  onStopGame() {
+    clearInterval(this.interval);
+  }
 }
